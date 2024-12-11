8ca4b8a881fbd25bd1d17835a5defd760963556a
svartfika <svartfika@proton.me>
2024-12-11 11:11:09 +0100

    added ELSE to CASE

--- exercises/exercise_1/sql/transform.sql
+++ exercises/exercise_1/sql/transform.sql
@@ -13,18 +13,21 @@ CREATE TABLE IF NOT EXISTS staging.cleaned_salaries AS (
             WHEN experience_level = 'MI' THEN 'Intermediate'
             WHEN experience_level = 'SE' THEN 'Expert'
             WHEN experience_level = 'EX' THEN 'Executive'
+            ELSE experience_level
         END AS experience_level,
         CASE
             WHEN employment_type = 'FT' THEN 'Full time'
             WHEN employment_type = 'PT' THEN 'Part time'
             WHEN employment_type = 'CT' THEN 'Contract'
             WHEN employment_type = 'FL' THEN 'Freelance'
+            ELSE employment_type
         END AS employment_type,
         remote_ratio,
         CASE
             WHEN company_size = 'S' THEN 'Small'
             WHEN company_size = 'M' THEN 'Medium'
             WHEN company_size = 'L' THEN 'Large'
+            ELSE company_size
         END AS company_size,
         round(salary_in_usd * getvariable('fx_rate_usd_sek')) ::INTEGER AS salary_annual_sek,
         round(salary_annual_sek / 12) ::INTEGER AS salary_monthly_sek,
@@ -33,6 +36,7 @@ CREATE TABLE IF NOT EXISTS staging.cleaned_salaries AS (
             WHEN salary_monthly_sek BETWEEN 50000 AND 100000 THEN 'Medium'
             WHEN salary_monthly_sek BETWEEN 100000 AND 200000 THEN 'High'
             WHEN salary_monthly_sek >= 200000 THEN 'Insanely high'
+            ELSE salary_monthly_sek
         END AS salary_level
     FROM ingest.raw_salaries
     -- WHERE job_title = 'Data Engineer' 
